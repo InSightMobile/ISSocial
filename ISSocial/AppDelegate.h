6296d5e8d3e33fe25aa91ee74a37a810fffa9370
yarry <yarryp@gmail.com>
2013-06-17 12:20:52 +0400

    Initial

---
+++ ISSocial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ISSocial
+//
+//  Created by Ярослав on 17.06.13.
+//  Copyright (c) 2013 Ярослав. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ISSocial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ISSocial
+//
+//  Created by Ярослав on 17.06.13.
+//  Copyright (c) 2013 Ярослав. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
