189a4d133b413fa039bdba894bec8c7b61f5a6e2
yarry <yarryp@gmail.com>
2013-09-06 12:23:17 +0400

    add basic vk link share

--- ISSocial/SocialConnectors/Vkontakte/VkontakteConnector+Link.m
+++ ISSocial/SocialConnectors/Vkontakte/VkontakteConnector+Link.m
@@ -8,6 +8,7 @@
 
 #import "VkontakteConnector+Link.h"
 #import "SLinkData.h"
+#import "SUserData.h"
 
 
 @implementation VkontakteConnector (Link)
@@ -20,13 +21,46 @@
 
         //pageId = link.linkURL.absoluteString;
 
-        [self simpleMethod:@"likes.add" parameters:@{@"item_id":pageId, @"page_url" : link.linkURL.absoluteString,@"type" : @"sitepage"}
-                 operation:operation processor:^(id response) {
+        [self simpleMethod:@"likes.add" parameters:@{@"item_id" : pageId, @"page_url" : link.linkURL.absoluteString, @"type" : @"sitepage"}
+                 operation:operation processor:^(id response)
+        {
 
             SLinkData *result = link;
             result.userLikes = @YES;
             result.likesCount = @([response[@"likes"] intValue]);
             [operation complete:result];
+        }];
+    }];
+}
+
+- (SObject *)publishLink:(SLinkData *)link completion:(SObjectCompletionBlock)completion
+{
+    return [self operationWithObject:link completion:completion processor:^(SocialConnectorOperation *operation)
+    {
+        NSString *userId = nil;
+        if (link.owner && link.owner.objectId != self.currentUserData.objectId) {
+            userId = link.owner.objectId;
+        }
+
+        NSMutableDictionary *params = [NSMutableDictionary dictionary];
+
+        if (link.linkURL) {
+            params[@"attachments"] = link.linkURL.absoluteString;
+        }
+
+        if (userId) {
+
+        }
+
+        [self simpleMethod:@"wall.post" parameters:params operation:operation processor:^(id result)
+        {
+            if (result[@"post_id"]) {
+                [operation complete:[SObject successful]];
+            }
+            else {
+                [operation completeWithFailure];
+            }
+
         }];
     }];
 }
